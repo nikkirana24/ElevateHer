8ebe5897cca6a910ea10f7a53ca864479e27d1c5
Nikki <nikkiranabhat@gmail.com>
2025-07-29 14:15:50 -0400

    Initial Commit

---
+++ ElevateHer/ElevateHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevateHerApp.swift
+//  ElevateHer
+//
+//  Created by Nikki  on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ElevateHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
